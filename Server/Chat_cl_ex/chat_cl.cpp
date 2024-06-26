bb2a21e1bb7d776e6c5ad52bbbac206f6d7f95a8
2002OID <ykkim308321@gmail.com>
2024-06-26 22:59:11 +0900

    주소 변경

--- Server/Chat_cl_ex/chat_cl.cpp
+++ Server/Chat_cl_ex/chat_cl.cpp
@@ -6,7 +6,7 @@
 #pragma comment (lib, "WS2_32.LIB")
 
 constexpr short PORT = 4000;
-constexpr char SERVER_ADDR[] = "127.0.0.1";
+constexpr char SERVER_ADDR[] = "172.30.1.35";
 constexpr int BUFSIZE = 256;
 
 bool bshutdown = false;
