7356893430432290d4a1896c97e8d788c2e29488
신창화 <sch9270@gmail.com>
2024-06-27 00:24:12 +0900

    DepthStencillBuffer
    完

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -53,7 +53,18 @@ void Game::Update()
 
 	{
 		Transform t;
-		t.offset = Vec4(0.f, 0.f, 0.f, 0.f);
+		t.offset = Vec4(0.f, 0.f, 0.2f, 0.0f);
+		mesh->SetTransform(t);
+
+		mesh->SetTexture(texture);
+
+		mesh->Render();
+	}
+
+
+	{
+		Transform t;
+		t.offset = Vec4(0.25f, 0.25f, 0.f, 0.f);
 		mesh->SetTransform(t);
 
 		mesh->SetTexture(texture);
