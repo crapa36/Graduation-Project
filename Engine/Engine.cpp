275ffdbe445069e9d9d830c5dbd25c51833df6a5
신창화 <sch9270@gmail.com>
2024-06-28 02:55:20 +0900

    Scene
    完

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -2,7 +2,9 @@
 #include "Engine.h"
 #include "Material.h"
 #include "Transform.h"
-
+#include "Input.h"
+#include "Timer.h"
+#include "SceneManager.h"
 
 void Engine::Init(const WindowInfo& info)
 {
@@ -18,8 +20,6 @@ void Engine::Init(const WindowInfo& info)
 	_rootSignature = make_shared<RootSignature>();
 	_tableDescHeap = make_shared<TableDescriptorHeap>();
 	_depthStencilBuffer = make_shared<DepthStencilBuffer>();
-	_input = make_shared<Input>();
-	_timer = make_shared<Timer>();
 
 	_device->Init();
 	_cmdQueue->Init(_device->GetDevice(), _swapChain);
@@ -27,36 +27,35 @@ void Engine::Init(const WindowInfo& info)
 	_rootSignature->Init();
 	_tableDescHeap->Init(256);
 	_depthStencilBuffer->Init(_window);
-	
-	_input->Init(info.hwnd);
-	_timer->Init();
 
 	CreateConstantBuffer(CBV_REGISTER::b0, sizeof(TransformMatrix), 256);
 	CreateConstantBuffer(CBV_REGISTER::b1, sizeof(MaterialParams), 256);
 
 	ResizeWindow(info.width, info.height);
+
+	GET_SINGLE(Input)->Init(info.hwnd);
+	GET_SINGLE(Timer)->Init();
+
+}
+
+void Engine::Update()
+{
+
+	GET_SINGLE(Input)->Update();
+	GET_SINGLE(Timer)->Update();
+
+	Render();
+
+	ShowFps();
 }
 
 void Engine::Render()
 {
 	RenderBegin();
 
-	// TODO : 나머지 물체를 그려준다.
+	GET_SINGLE(SceneManager)->Update();
 
 	RenderEnd();
-}
-
-void Engine::Update()
-{
-	_input->Update();
-	_timer->Update();
-
-	ShowFps();
-}
-
-void Engine::LateUpdate()
-{
-	//TODO
 }
 
 void Engine::RenderBegin()
@@ -82,7 +81,7 @@ void Engine::ResizeWindow(int32 width, int32 height)
 
 void Engine::ShowFps()
 {
-	uint32 fps = _timer->GetFps();
+	uint32 fps = GET_SINGLE(Timer)->GetFps();
 
 	WCHAR text[100] = L"";
 	::wsprintf(text, L"FPS : %d", fps);
--- Engine/Engine.h
+++ Engine/Engine.h
@@ -10,8 +10,6 @@
 #include "TableDescriptorHeap.h"
 #include "Texture.h"
 #include "DepthStencilBuffer.h"
-#include "Input.h"
-#include "Timer.h"
 #include "Material.h"
 
 class Engine
@@ -19,12 +17,7 @@ class Engine
 public:
 
 	void Init(const WindowInfo& info);
-	void Render();
-
-public:
-
 	void Update();
-	void LateUpdate();
 
 public:
 	shared_ptr<Device>	GetDevice() { return _device; };
@@ -34,14 +27,12 @@ public:
 	shared_ptr<TableDescriptorHeap>	GetTableDescHeap() { return _tableDescHeap; };
 	shared_ptr<DepthStencilBuffer>	GetDepthStencilBuffer() {return _depthStencilBuffer; }
 
-	shared_ptr<Input> GetInput() { return _input; }
-	shared_ptr<Timer> GetTimer() { return _timer; }
-
 	shared_ptr<ConstantBuffer> GetConstantBuffer(CONSTANT_BUFFER_TYPE type) { return _constantBuffers[static_cast<uint8>(type)]; }
 
 
 
 public:
+	void Render();
 	void RenderBegin();
 	void RenderEnd();
 
@@ -62,9 +53,6 @@ private:
 	shared_ptr<RootSignature>		 _rootSignature;
 	shared_ptr<TableDescriptorHeap>		 _tableDescHeap;
 	shared_ptr<DepthStencilBuffer>	_depthStencilBuffer;
-
-	shared_ptr<Input> _input;
-	shared_ptr<Timer> _timer;
 
 	vector<shared_ptr<ConstantBuffer>> _constantBuffers;
 
