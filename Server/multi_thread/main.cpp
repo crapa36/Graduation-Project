e7958b3d1165174dc15de09bf1d3ce075275fce5
2002OID <ykkim308321@gmail.com>
2024-07-03 12:01:11 +0900

    성능측정 예시코드

--- Server/multi_thread/main.cpp
+++ Server/multi_thread/main.cpp
@@ -1,6 +1,7 @@
 #include <thread>
 #include <iostream>
 #include <vector>
+#include <chrono>
 using namespace std;
 
 void ThreadFunc(int threadid)
@@ -11,7 +12,11 @@ void ThreadFunc(int threadid)
 int main()
 {
 	vector <thread> my_thread;
+	//시간측정
+	auto t = chrono::high_resolution_clock::now();
 	for (int i = 0; i < 10; ++i)
 		my_thread.emplace_back(ThreadFunc, i);
+	auto d = chrono::high_resolution_clock::now() - t;
+	cout << chrono::duration_cast<chrono::milliseconds>(d).count() << " msecs\n";
 	for (auto& t : my_thread) t.join();
 }
