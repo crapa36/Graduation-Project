105032bc476e1612b189901f444deb9e44b69815
2002OID <ykkim308321@gmail.com>
2024-07-03 13:46:08 +0900

    멀티쓰레드

--- Server/multi_thread/main.cpp
+++ Server/multi_thread/main.cpp
@@ -3,28 +3,30 @@
 #include <vector>
 #include <chrono>
 using namespace std;
-
+const auto  MAX_THREADS = 64;
 
 volatile int sum{};
 
-void ThreadFunc()
+void ThreadFunc(int num_of_thread)
 {
-	for (auto i = 0; i < 25000000; ++i) {
+	for (auto i = 0; i < 50000000 / num_of_thread; ++i) {
 		sum = sum + 2;
 	}
 }
 
 int main()
 {
-	
+	vector<thread> threads;
 	//시간측정
-	auto t = chrono::high_resolution_clock::now();
-	thread t1{ ThreadFunc };
-	thread t2{ ThreadFunc };
-	auto d = chrono::high_resolution_clock::now() - t;
-	t1.join();
-	t2.join();
-	cout << "Sum = " << sum <<  " duration = ";
-	cout << chrono::duration_cast<chrono::milliseconds>(d).count() << " msecs\n";
+	for (auto i = 1; i <= MAX_THREADS; i *= 2) {
+		sum = 0;
+		threads.clear();
+		auto start = chrono::high_resolution_clock::now();
+		for (auto j = 0; j < i; ++j) threads.push_back(thread{ ThreadFunc, i });
+		for (auto& tmp : threads) tmp.join();
+		auto duration = chrono::high_resolution_clock::now() - start;
+		cout << i << " Threads,   Sum = " << sum;
+		cout << "  Duration = " << chrono::duration_cast<chrono::milliseconds>(duration).count() << " milliseconds\n";
+	}
 	
 }
