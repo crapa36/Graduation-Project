f514e4cf85e47c173c9d513fe3e67e205743274e
2002OID <ykkim308321@gmail.com>
2024-07-03 13:15:08 +0900

    듀얼쓰레드

--- Server/multi_thread/main.cpp
+++ Server/multi_thread/main.cpp
@@ -4,21 +4,27 @@
 #include <chrono>
 using namespace std;
 
+
+volatile int sum{};
+
 void ThreadFunc()
 {
-	
+	for (auto i = 0; i < 25000000; ++i) {
+		sum = sum + 2;
+	}
 }
-
-volatile  int sum{};
 
 int main()
 {
-	vector <thread> my_thread;
+	
 	//시간측정
 	auto t = chrono::high_resolution_clock::now();
-	for (auto i = 0; i < 50000000; ++i) sum = sum + 2;
+	thread t1{ ThreadFunc };
+	thread t2{ ThreadFunc };
 	auto d = chrono::high_resolution_clock::now() - t;
+	t1.join();
+	t2.join();
 	cout << "Sum = " << sum <<  " duration = ";
 	cout << chrono::duration_cast<chrono::milliseconds>(d).count() << " msecs\n";
-	for (auto& t : my_thread) t.join();
+	
 }
