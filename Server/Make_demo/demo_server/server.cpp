2441303da6c2ba416ca7db73f7e5fee746434264
2002OID <ykkim308321@gmail.com>
2024-07-15 11:45:45 +0900

    소켓 및 서버 주소 설정

--- Server/Make_demo/demo_server/server.cpp
+++ Server/Make_demo/demo_server/server.cpp
@@ -3,6 +3,7 @@
 
 #pragma comment (lib, "WS2_32.LIB")
 
+constexpr short PORT = 4000;
 
 int main() {
 	std::wcout.imbue(std::locale("korean"));
@@ -12,6 +13,15 @@ int main() {
 	if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0) {
 		std::cout << "WSAStartup이 정상적으로 호출 되지 않았습니다" << std::endl;
 	}
+	//소켓 및 서버 주소 설정
+	SOCKET server_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, WSA_FLAG_OVERLAPPED);
+	SOCKADDR_IN server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	bind(server_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr));
+	listen(server_socket, SOMAXCONN);
 
 	WSACleanup();				//종료시 호출
 }
