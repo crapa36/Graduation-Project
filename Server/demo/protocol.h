15f5acf5734ad3812bf5bc9fe1cffa46d7589d5b
2002OID <ykkim308321@gmail.com>
2024-09-28 04:55:33 +0900

    플레이어 이름, 삭제에 대한 프로토콜

--- Server/demo/protocol.h
+++ Server/demo/protocol.h
@@ -1,7 +1,7 @@
 constexpr int PORT_NUM = 4000;
 constexpr int BUF_SIZE = 200;
-
 constexpr int MAX_USER = 10;
+constexpr int MAX_NAME_SIZE = 20;
 
 
 
@@ -10,4 +10,10 @@ constexpr char CS_IN = 0;
 constexpr char CS_MOVE = 1;
 constexpr char CS_ROTATION = 2;
 
+constexpr char SC_REMOVE_PLAYER = 4;
 
+struct SC_REMOVE_PLAYER_PACKET {
+	unsigned char size;
+	char	type;
+	short	id;
+};
